65b4d2000a0d66d3d777515f327337641c6d235e
Vedran Mornar <vedran@fer.hr>
2024-08-31 22:30:34 +0200

    filter patched

--- components/table.vue
+++ components/table.vue
@@ -372,9 +372,10 @@ export default {
          * @returns {any} - The converted value.
          */
         realValue(col, val) {
+            console.log(col.type, val);
             if (col.type == "timestamp with time zone") {
                 return this.toLocalISOString(new Date(val));
-            } else if (col.type == "text" || col.type == "character varying") {
+            } else if (col.type == "string" || col.type == "text" || col.type == "character varying") {
                 return val?.toLowerCase();
             } else {
                 return val;
